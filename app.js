e88fa60d716bc11b4c776bced8e4f2da7d5af7c1
Amro Hassan <79529406+hass0319@users.noreply.github.com>
2025-05-28 15:31:35 +0300

    added app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector(".form-box");
+  const billInput = document.getElementById("bill");
+  const tipInput = document.getElementById("tip");
+  const totalDisplay = document.getElementById("total").querySelector("span");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const bill = parseFloat(billInput.value);
+    const tipPercent = parseFloat(tipInput.value);
+
+    if (isNaN(bill) || isNaN(tipPercent) || bill < 0 || tipPercent < 0) {
+      alert("Please Enter a number.");
+      return;
+    }
+
+    const tip = bill * (tipPercent / 100);
+    const total = bill + tip;
+    console.log(total);
+    totalDisplay.textContent = `$${total.toFixed(2)}`;
+    totalDisplay.style.color = "red";
+  });
+});
